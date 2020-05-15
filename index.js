487ef084cf3f7e4c176d31bb90ac189ec0a47551
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-05-15 13:45:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function unleashDog(dogName, dogBreed) {
 }
 let routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
-function exerciseDog(dogName, dogBreed,routine){
+function exerciseDog(dogName, dogBreed){
   for(let i=0;i<routine.length;i++){
     routine[i](dogName,dogBreed)
   }
